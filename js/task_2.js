dc8e8402c1fb052454823feaba0870a33035d03f
Oleg Soldatenko <sunskays@gmail.com>
2020-12-09 14:50:02 +0200

    Task #2

--- js/task_2.js
+++ js/task_2.js
@@ -5,8 +5,14 @@
 'use strict';
 
 const countProps = function(obj) {
-  // твой код
+    const arrayKeys = Object.keys(obj);
+    const sumIndex = arrayKeys.length;
+    console.log(`Колличество свойств в объекте: ${sumIndex}`);
 };
 
 
-console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+countProps({}); // 0
+
+countProps({ name: 'Mango', age: 2 }); // 2
+
+countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }); // 3
