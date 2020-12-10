ded3e3155629771ed9c6ac2574ab6c35867c329a
fenogen <sunskays@gmail.com>
2020-12-10 15:09:47 +0200

    Task #7

--- js/task_7.js
+++ js/task_7.js
@@ -1,12 +1,15 @@
 // Напиши скрипт управления личным кабинетом интернет банка.
 // Есть объект account в котором необходимо реализовать методы для работы с балансом и историей транзакций.
 
+'use strict';
 
-'use strict'; 
+let amount = 0;
+let id = 0;
+let type = 0;
+let message;
+let messageMain;
+let date = new Date();
 
-//  Типов транзацкий всего два.
-//  Можно положить либо снять деньги со счета.
-// Каждая транзакция это объект со свойствами: id, type и amount
 const Transaction = {
   DEPOSIT: 'deposit',
   WITHDRAW: 'withdraw',
@@ -14,100 +17,112 @@ const Transaction = {
 };
 
 const account = {
-  balance: 0,       // Текущий баланс счета
-  transactions: []  // История транзакций
+  balance: 100, // Текущий баланс счета
+  transactions: [{ id, type, amount, date }], // История транзакций
 };
 
-let amount = 0; 
-let id = 0;
-let type = 0
-let message;
+console.log('Question #1');
 
-message = prompt('Введите сумму для ПОПОЛНЕНИЯ денежных средств');
+while (true || false) {
+  messageMain = confirm('Вы хотите пополнить свой счет?');
 
-const createTransaction = function (amount, type) {
-  while (message === null) {
-    alert('Отменено пользователем');
-    return;
+  if (messageMain === true) {
+    console.log('hello I am your DEPOSIT-fuction');
+    message = prompt('Введите сумму для ПОПОЛНЕНИЯ денежных средств');
+
+    const deposite = function (amount) {
+      while (message >= 1) {
+        amount = Number(message);
+        account.balance += amount;
+
+        id = id + 0.01;
+        type = Transaction.DEPOSIT;
+        account.transactions.push({ id, type, amount, date });
+        alert(
+          `Вы успешно пополнили ваш счет на ${amount} $ . Ваш баланс = ${account.balance} $`,
+        );
+        return;
+      }
+      if (message === null) {
+        alert('Отменено пользователем');
+        return;
+      }
+      if (message !== Number(message)) {
+        alert('Было введено не число, попробуйте еще раз');
+        return;
+      }
+    };
+    deposite(amount);
   }
-  if (message > 1) {
-    amount = Number(message);
-    account.balance = amount;
-    id = id + 0.01;
-    type = Transaction.DEPOSIT;
-    account.transactions.push({ id, type, amount });
-    alert(`Вы успешно пополнили ваш счет на ${amount} $ . Ваш баланс = ${account.balance} $`);
-    console.log(account.transactions);
-    return;
-  }
 
-  alert('Было введено не число, попробуйте еще раз');
-  message = prompt('Введите сумму для пополнения');
+  if (messageMain === false) {
+    console.log('Question #2');
+    messageMain = confirm('Вы хотите снять свои деньги?');
+
+    if (messageMain === true) {
+      console.log('hello I am your WITHDRAW-fuction');
+      message = prompt('Введите сумму для СНЯТИЯ денежних средств');
+
+      const withdraw = function (amount) {
+        while (message === null) {
+          alert('Отменено пользователем');
+          return;
+        }
+        if (message > 1 && account.balance < message) {
+          amount = Number(message);
+          alert(
+            `Cнятие такой суммы не возможно, недостаточно средств на счету. Ваш баланс всего: ${account.balance}`,
+          );
+          id = id + 0.01;
+          type = 'Error';
+          amount = 0;
+          account.transactions.push({ id, type, amount, date });
+          // console.table(account.transactions);
+          return;
+        }
+        if (message > 1 && account.balance >= message) {
+          amount = Number(message);
+          account.balance = account.balance - amount;
+          alert(`Деньги успешно сняты. Ваш баланс = ${account.balance} $`);
+
+          id = id + 0.01;
+          type = Transaction.WITHDRAW;
+          account.transactions.push({ id, type, amount, date });
+          // console.table(account.transactions);
+          return;
+        }
+        if (message !== Number(message)) {
+          alert('Было введено не число, попробуйте еще раз');
+          return;
+        }
+      };
+      withdraw(amount);
+    } 
+  
+    if (messageMain === false) {
+      console.log('Question #3');
+      messageMain = confirm('Вы хотите посмотреть свой баланс?');
+    
+      if (messageMain === true) {
+        console.log('hello I am your BALANCE-fuction');
+        alert(`Ваш баланс = ${account.balance}`);
+      }
+    
+      if (messageMain === false) {
+        console.log('Question #4');
+        messageMain = confirm('Вы хотите посмотреть всю историю транзакций?');
+      
+        if (messageMain === true) {
+          console.log('hello I am your Transaction-fuction');
+          console.table(account.transactions);
+          break;
+        }
+        if (messageMain === false) {
+          console.log('Good bye');
+          alert('Досвидание')
+          break;
+        }
+      }
+    }
+  }
 }
-
-createTransaction(amount, type);
-
-  /*
-   * Метод отвечающий за добавление суммы к балансу.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций
-   */
-
-message = prompt('Введите сумму для СНЯТИЯ денежних средств');
-
-const deposit = function (amount) {
-  while (message === null) {
-    alert('Отменено пользователем');
-    return;
-  }
-  if (message > 1 && account.balance < message) {
-    amount = Number(message);
-    alert(`Cнятие такой суммы не возможно, недостаточно средств на счету. Ваш баланс всего: ${account.balance}`);
-    id = id + 0.01;
-    type = 'Error';
-    amount = 0;
-    account.transactions.push({ id, type, amount });
-    console.log(account.transactions);
-    return;
-  } if (message > 1 && account.balance > message) {
-    amount = Number(message);
-    account.balance = account.balance - amount;
-    alert(`Деньги успешно сняты. Ваш баланс = ${account.balance} $`);
-    id = id + 0.01;
-    type = Transaction.WITHDRAW;
-    account.transactions.push({ id, type, amount });
-    console.table(account.transactions);
-    return;
-  }
-  alert('Было введено не число, попробуйте еще раз');
-  // message = prompt('Введите сумму для СНЯТИЯ денежних средств еще раз');
-}
-
-deposit(amount);
-
-//    * Метод отвечающий за снятие суммы с баланса.
-//    * Принимает сумму танзакции.
-//    * Вызывает createTransaction для создания объекта транзакции
-//    * после чего добавляет его в историю транзакций.
-//    *
-//    * Если amount больше чем текущий баланс, выводи сообщение
-//    * о том, что снятие такой суммы не возможно, недостаточно средств.
-
-const withdraw = function(amount) {};
-//    * Метод возвращает текущий баланс
-
-
-//   getBalance() {},
-
-//   /*
-//    * Метод ищет и возвращает объект транзации по id
-//    */
-//   getTransactionDetails(id) {},
-
-//   /*
-//    * Метод возвращает количество средств
-//    * определенного типа транзакции из всей истории транзакций
-//    */
-//   getTransactionTotal(type) {},
-// };
